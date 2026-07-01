1ee4472417ed2e0350779f4442d179733d6e467a
BobaSpacetime <163637926+BobaSpacetime@users.noreply.github.com>
2026-07-01 19:29:26 +0200

    Add files via upload

--- circle.py
+++ circle.py
@@ -16,6 +16,18 @@ class circ():
     def movingup(self):
         self.y -= 10
         pygame.draw.circle(screen, self.color, (self.x, self.y), self.r)
+    def movingdown(self):
+        self.y += 10
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.r)
+    def movingleft(self):
+        self.x -= 10
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.r)
+    def movingright(self):
+        self.x += 10
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.r)
+    def sizeup(self):
+        self.r += 5
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.r)
 
 circl = circ(325, 225, 30, "white")
 
@@ -26,8 +38,19 @@ while True:
             pygame.quit()
         if event.type == pygame.MOUSEBUTTONDOWN:
             circl.drawpi()
+        if event.type == pygame.MOUSEBUTTONUP:
+            circl.sizeup()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
                 screen.fill("#100821FF")
                 circl.movingup()
+            if event.key == pygame.K_DOWN:
+                screen.fill("#100821FF")
+                circl.movingdown()
+            if event.key == pygame.K_LEFT:
+                screen.fill("#100821FF")
+                circl.movingleft()
+            if event.key == pygame.K_RIGHT:
+                screen.fill("#100821FF")
+                circl.movingright()
     pygame.display.update()
